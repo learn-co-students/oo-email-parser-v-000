4096d960b064d8f9d8d1ea50b3b48d35f128c26d
ed-karabinus <ejkarabin@gmail.com>
2015-12-04 00:37:55 -0600

    Finished OO Email Parser; added class EmailParser and methods #emails, #emails=, #initialize, and #parse to definition of class in lib/email_parser.rb

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,21 @@
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
+
+class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    self.emails = emails
+  end
+
+  def parse
+    unique_emails = []
+    @emails.split(/(,|\s)+/).each do |email| 
+      if email != " " && !(unique_emails.include?(email))
+        unique_emails << email
+      end
+    end
+    unique_emails
+  end
+end
