6654d8e37f818ee2988bd3238baa74c01faed0e0
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-12 22:04:28 +0000

    finishes object oriented email parser lab

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,17 @@
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
+class EmailParser
+  attr_accessor :parse
+   
+  def initialize(emails)
+    @emails = emails 
+  end
+   
+  def parse
+    # splits emails by comma or space, strips whitespace and duplicates
+    # and deletes any empty strings in the array.
+    @emails.split(/[, ]/).map(&:strip).uniq.delete_if {|t| t == ""}
+  end
+
+end
