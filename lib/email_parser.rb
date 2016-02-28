10566f0de7ad175c2dbb33507e4ce817dc54726d
jwolfe890 <jwolfe890@gmail.com>
2016-02-28 18:07:48 -0500

    emailparser

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,35 @@
-# Build a class EmailParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
+class EmailParser
+
+  attr_accessor :regular_list 
+
+  # def regular_list=(regular_list)
+  #   @regular_list = regular_list
+  # end 
+
+  # def regular_list
+  #   @regular_list
+  # end 
+
+  # def initialize(regular_list)
+  #     @regular_list = regular_list
+  # end 
+
+# why do you have to set regular_list equal 
+# to an instance variable if it's already set to that
+# in the setter and getter methods  
+
+  def parse 
+    regular_list.gsub(",","").split(" ").uniq 
+  end 
+
+end
+
+# "avi@test.com, arel@test.com test@avi.com, test@arel.com"
+
+emails = EmailParser.new
+
+emails.regular_list = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
+
+puts emails.parse
+
+# puts "avi@test.com, arel@test.com test@avi.com, test@arel.com".gsub(",","").split(" ").uniq
---
+++ spec/rubytest.rb
@@ -0,0 +1,3 @@
+puts " a@test.com, b@tast.com c@test.com, d@test.com, e@test.com f@test.com".split(/[aeiou]+/).length
+
+
