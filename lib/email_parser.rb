92b00535193bd91809edb6732c5da8507685117b
PeteAJ <peter.jacobson@gmail.com>
2016-03-21 14:03:31 -0400

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,13 @@
-class Song
-  attr_accessor :email
+class EmailParser
+  attr_reader :emails
 
-def parse 
-end
+  def initialize(emails)
+    @emails = emails
+  end
 
+  def parse
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
+  end
 end
