78aa23bdf46d0727fce5eb24b0fc53a7045f5e67
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-15 16:51:52 +0000

    edited my comments

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,16 +3,17 @@ class EmailParser
   attr_accessor  :emails
 
   def initialize(emails)
-    @emails = emails
+    @emails = emails #initialize the "emails" string so that it is created everytime a new object is instanciated
   end
 
   def parse
+    #to make it easy to shovel them individually without extra whitespace or commas
     @emails_array=[] #create an empty array that can hold the parsed data
     @emails.delete!(',') # delete all commas
-    @emails.split.each do |email| #add commas after every seperated word
+    @emails.split.each do |email| #add commas after every seperate email
       @emails_array << email
     end
     @emails_array.uniq
   end
-  
+
 end
