de5e90418c777f9fe271c05c1a97ce1b8429404c
andrewmmarquis <andrewmmarquis@gmail.com>
2016-07-12 15:46:05 -0400

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,16 @@
 class EmailParser
 
- 
+  attr_reader :emails
 
+  def initialize(emails)
+    @emails = emails 
+  end
+
+  def parse
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
+  end
 end
 
 
