d3e481244d65c8c3d02c269b53315ab698acda33
Elias Ventura <eliasventura23@gmail.com>
2017-05-11 01:12:32 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,14 @@
 class EmailParser
+
   attr_accessor :emails
 
   def initialize(emails)
-    @emails = emails
+    @emails= emails
   end
 
   def parse
     emails.split.map do |email|
-    email.split(',')
-  end.flatten.uniq
-end
+      email.split(",")
+    end.flatten.uniq
+  end
 end
