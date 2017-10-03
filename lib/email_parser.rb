d009a6f37614b6f41159a3ef5d4671af2903747d
Jerry Boyce <jokerdelta@yahoo.com>
2017-10-03 02:52:50 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,20 +7,20 @@
 class EmailParser
 
   attr_accessor :emails
-  @emails = ""
-  @email_array = []
+  @emails = "" # initialize empty string
+  @email_array = [] #initialize empty array
 
   def initialize(emails)
-    @emails = emails
-    self.parse
+    @emails = emails # set instance variable
+    self.parse # call instance method "parse"
   end
 
   def parse
      @email_array = @emails.split(/[\s,]+/)
     #split based on mathcing any character in the set
     #whitespace or comma, match one or more of the preceding token
-    @email_array.uniq!
-    @email_array
+    @email_array.uniq! #remove duplicate elements from array
+    @email_array # return updated array
   end
 
 end
