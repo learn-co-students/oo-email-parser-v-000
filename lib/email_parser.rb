17175c13f515de0b42c3b66acc48188d3db9e214
Felix Lemerle <felix@lemerle.fr>
2017-11-10 23:12:06 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,10 +5,10 @@
 
 class EmailParser
   def initialize(emails)
-    @@emails = emails
+    @emails = emails
   end
 
   def parse
-    @@emails.split(/,* /).uniq
+    @emails.split(/,* /).uniq
   end
 end
