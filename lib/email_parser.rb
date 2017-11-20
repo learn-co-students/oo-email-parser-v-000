5eb655b3330336dc219ee476c25762b5c781cf43
charityyoungblood <youngblood.charity@gmail.com>
2017-11-20 15:56:07 -0800

    added emails as an argument to initialize method

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,23 @@
+require 'pry'
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
-
+binding.pry 
 class EmailParser 
+  attr_accessor :email_address 
+  @@email_list = []
+
+  def initialize(emails)
+    @@email_list << self 
+  end
   # create instance method - parse - that returns an array
-  def parse(emails) 
+  def self.parse(emails)
+    binding.pry
     # should take a string of emails, return an array of emails in correct format
-    
+    email = self.new 
+    email.email_address = emails
+    emails.split(" , ")
   end 
   
   
