79e7fdca35969505ffef0851907b4d4634cbf1e7
charityyoungblood <youngblood.charity@gmail.com>
2017-11-20 17:14:52 -0800

    added comments on what return value should be

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,14 +8,21 @@ class EmailParser
   attr_accessor :email_address 
   @@email_list = []
 
-  def initialize(email)
+  def initialize(emails)
+   @email_address = emails 
+    @@email_list << self
     
   end
   # create instance method - parse - that returns an array
- 
-  def parse
-   # csv.split(",")
-   
+  def all 
+    @@email_list 
+  end
+  
+  def parse 
+    binding.pry
+   # separate emails by comma and return an array
+  
+   @@email_list.detect { |email_check| email_check.split(",")}
     
     # should take a string of emails, return an array of emails in correct format
    
@@ -23,3 +30,5 @@ class EmailParser
   
   
 end 
+
+#parse = EmailParser.new(emails)
