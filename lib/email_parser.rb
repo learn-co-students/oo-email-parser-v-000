c8df1a2c4fe272f72efca49ac7cd6de99552a63c
Josh Kramer <32785040+funemployeddad@users.noreply.github.com>
2018-01-02 10:31:33 -0700

    Update email_parser.rb

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,19 @@
 # or whitespace (' ').
 
 
+
 class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    list = emails.split(/[ ,]/)
+    list.delete("")
+    list.uniq
+  end
 
 
 end
