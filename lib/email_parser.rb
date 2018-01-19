2cd0a01c08794e6ec28dd5acbb4781b4f0e00654
jljardon <github email address>
2018-01-18 22:41:19 -0500

    add parse methods

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,16 @@
-# Build a class EmailParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+require 'pry'
+class EmailParser
+   attr_accessor :emails
+
+   def initialize(emails)
+      @emails = emails
+      parse
+   end
+
+   def parse
+      parsed_emails = []
+      parsed_emails = @emails.split(/[\s,]+/)
+      parsed_emails.uniq
+   end
+end
+ # binding.pry
