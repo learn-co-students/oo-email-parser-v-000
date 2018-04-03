62831d66dab41f15c8a546060070e6c65b4f86c1
Eric Johnson <ericjohnson0216@icloud.com>
2018-04-03 00:37:04 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,21 +3,38 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser
-  attr_accessor :email
+# class EmailParser
+#   attr_accessor :email
+#
+#   def initialize(email)
+#     @email = email
+#   end
+#
+#   def parse
+#     emails.split.map do |email|
+#       email.split(',')
+#     end.flatten.uniq
+#   end
+# end
+#
+#
+#     #where does the string containing emails come from?
+#
+#     #what character do we need to parse on
+#     #what is self?
+#     #self is an instance when it is an instance method
+#     #self is the class when it is a class method (def self.method_name... end)
 
-  def initialize(email)
-    @email = email
+class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
   end
 
   def parse
-    email.split(" ")
-    #where does the string containing emails come from?
-
-    #what character do we need to parse on
-    #what is self?
-    #self is an instance when it is an instance method
-    #self is the class when it is a class method (def self.method_name... end)
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
   end
-
 end
