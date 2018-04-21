c4249bb49998ead63904ad16379a7ce8146b49e7
Erica Murdock-Waters <ericahpa@gmail.com>
2018-04-21 21:25:29 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,7 @@
 # email_parser.rb
 # Build a class EmailParser that accepts string of unformatted emails
 class EmailParser
-  attr_accessor :email, :email_parser
+  attr_accessor :email_parser
 
   def initialize(email_file)
     @email_parser = email_file
@@ -12,5 +12,4 @@ class EmailParser
     @email_parser = email_parser.scan(/\w+@\w+.\w+/)
     @email_parser.uniq
   end
-  
 end
