466e54d8678e48bf6209a6bc50a1526e2a9c836c
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-07 12:55:49 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,11 +9,25 @@ class EmailParser
   def initialize(emails)
     @emails = emails
   end
-
   # i will be parsing emails next =
   def parse
     array = emails.gsub(",", "").split(" ").uniq
   end
-
 end
 #binding.pry
+
+=begin
+class EmailParser
+  attr_reader :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
+  end
+end
+=end
