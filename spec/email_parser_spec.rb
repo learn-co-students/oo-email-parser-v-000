7b93e730fd6a942e7ad3012875202af84c31952c
Brandon Brasson <rocksong622@gmail.com>
2019-01-25 02:22:58 +0000

    Done.

--- spec/email_parser_spec.rb
+++ spec/email_parser_spec.rb
@@ -1,19 +1,19 @@
-puts describe "EmailParser" do
+ describe "EmailParser" do
   describe '#parse' do
     it "parses CSV emails" do
       expect(EmailParser.new("avi@test.com, arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
     end
 
-    puts it "parses space delimited emails" do
+     it "parses space delimited emails" do
       expect(EmailParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
     end
 
-    puts it "parses both CSV and space delimited emails" do
+     it "parses both CSV and space delimited emails" do
       emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
       expect(EmailParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"])
     end
 
-    puts it 'parses and removes duplicate emails' do
+     it 'parses and removes duplicate emails' do
       expect(EmailParser.new("avi@test.com, avi@test.com").parse).to eq(["avi@test.com"])
     end
   end
