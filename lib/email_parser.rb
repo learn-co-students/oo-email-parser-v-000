e2b8d7daa39b39d053a7e720205590c32465d76d
Eva Navarro <emanao@me.com>
2019-11-11 19:38:01 +0000

    Update parse method: robuster implementation

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,6 +8,7 @@ class EmailParser
     @emails=emails
   end
   def parse
-    emails.split(/,\s|\s/).uniq
+    #emails.split(/,\s|\s/).uniq
+    emails.split(/,|\s/).reject{|email| email==""}.uniq
   end
 end
